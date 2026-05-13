b8758a2cd2d2075e615d46a1804e4e4de2db1090
Thierry Delafontaine <delafthi@pm.me>
2026-05-13 13:18:31 +0200

    packages/mattpocock-skills: 0-unstable-2026-04-03 -> 0-unstable-2026-05-13

--- packages/mattpocock-skills/package.nix
+++ packages/mattpocock-skills/package.nix
@@ -6,20 +6,22 @@
 }:
 stdenvNoCC.mkDerivation (_finalAttrs: {
   pname = "mattpocock-skills";
-  version = "0-unstable-2026-04-03";
+  version = "0-unstable-2026-05-13";
 
   src = fetchFromGitHub {
     owner = "mattpocock";
     repo = "skills";
-    rev = "b843cb5ea74b1fe5e58a0fc23cddef9e66076fb8";
-    sha256 = "sha256-qOhU5bBnT6kI8c7i0r0IyecrgLJNNPlmQtAb6qWM73Q=";
+    rev = "f304057d61d3df3c9fd992ac2b6e3833cb9325fb";
+    sha256 = "sha256-jdUTec3217Bc+h5npjKOlIBHp3rhEd/sRKzDV2N7XIc=";
   };
 
   installPhase = ''
     cp -r . $out
   '';
 
-  passthru.updateScript = nix-update-script { };
+  passthru.updateScript = nix-update-script {
+    extraArgs = [ "--version=branch" ];
+  };
 
   meta = {
     description = "Skills for Real Engineers";
