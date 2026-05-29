71a0d3b3bb033816f442a03480044f86a47f5354
Thierry Delafontaine <delafthi@pm.me>
2026-05-29 23:30:24 +0200

    home/security/proton-pass-agent: disable telemetry

--- home/shared/security/proton-pass-agent.nix
+++ home/shared/security/proton-pass-agent.nix
@@ -10,5 +10,6 @@
     lib.optionalAttrs (pkgs.stdenv.hostPlatform.isLinux && osConfig.system.gui.enable)
       {
         PROTON_PASS_LINUX_KEYRING = "dbus";
+        PROTON_PASS_DISABLE_TELEMETRY = 1;
       };
 }
