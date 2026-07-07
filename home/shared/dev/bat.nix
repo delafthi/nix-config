8e0c352752c1227abca23169e5f4f629e4847d7c
Thierry Delafontaine <delafthi@pm.me>
2026-07-07 18:44:15 +0200

    home/dev/bat: drop backslash before `[` in MANPAGER sed regex

--- home/shared/dev/bat.nix
+++ home/shared/dev/bat.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
   home.sessionVariables = {
-    MANPAGER = "sh -c 'sed -u -e \\\"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\\\" | bat -p -lman'";
+    MANPAGER = "sh -c 'sed -u -e \\\"s/\\x1B[[0-9;]*m//g; s/.\\x08//g\\\" | bat -p -lman'";
     PAGER = "bat -p";
   };
   programs.bat = {
