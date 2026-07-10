e3376e6252ee5ef6e898f86d68beaea24ed0a056
Thierry Delafontaine <delafthi@pm.me>
2026-07-10 13:45:34 +0200

    home/linux/apps/zathura: fix font config
    Set font to the monospace default and set size to 14.

--- home/linux/apps/zathura.nix
+++ home/linux/apps/zathura.nix
@@ -3,7 +3,7 @@
   programs.zathura = {
     inherit (osConfig.system.gui) enable;
     options = {
-      font = builtins.head config.fonts.fontconfig.defaultFonts.sansSerif;
+      font = "${builtins.head config.fonts.fontconfig.defaultFonts.monospace} normal 14";
       selection-clipboard = "clipboard";
     };
   };
