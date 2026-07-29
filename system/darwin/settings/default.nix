702538e08941e7c1a54c418ccd8e5072c9d76d0c
Thierry Delafontaine <delafthi@pm.me>
2026-07-29 08:38:24 +0200

    system/darwin/settings: remove x86_64-darwin support and remove automatic rosetta installer

--- system/darwin/settings/default.nix
+++ system/darwin/settings/default.nix
@@ -3,7 +3,6 @@
     ./locales.nix
     ./macos-updates.nix
     ./quarantine.nix
-    ./rosetta.nix
     ./safari.nix
     ./screensaver.nix
   ];
--- system/darwin/settings/rosetta.nix
+++
@@ -1,8 +0,0 @@
-{
-  nix.extraOptions = ''
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
-  system.activationScripts.installRosetta.text = ''
-    softwareupdate --install-rosetta --agree-to-license
-  '';
-}
