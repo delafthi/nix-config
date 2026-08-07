7daa77679e059e4948cb3acd87283ee8c4a677af
Thierry Delafontaine <delafthi@pm.me>
2026-08-08 00:23:15 +0200

    system/darwin/linux-builder: use linux-builder with default configuration

--- system/darwin/linux-builder.nix
+++ system/darwin/linux-builder.nix
@@ -1,12 +1,3 @@
 {
-  nix.linux-builder = {
-    enable = true;
-    config = {
-      virtualisation = {
-        darwin-builder.memorySize = 4 * 1024;
-        cores = 4;
-      };
-    };
-    ephemeral = true;
-  };
+  nix.linux-builder.enable = true;
 }
