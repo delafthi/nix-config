d539afa6fc4c3602e6c01f38a9257d748b205e7a
Thierry Delafontaine <delafthi@pm.me>
2026-08-11 17:22:01 +0200

    packages/proton-drive: 3.0.0 -> 3.0.2

--- packages/proton-drive/package.nix
+++ packages/proton-drive/package.nix
@@ -11,14 +11,14 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "proton-drive";
-  version = "3.0.0";
+  version = "3.0.2";
 
   __structuredAttrs = true;
   strictDeps = true;
 
   src = fetchurl {
     url = "https://proton.me/download/drive/macos/${finalAttrs.version}/ProtonDrive-${finalAttrs.version}.dmg";
-    hash = "sha256-uMK4uGucUmwCwye9J9JbJ5zmDtlhVPTtMEQC8ukRZH8=";
+    hash = "sha256-xuYgBT4U5JLuQEHec78GvUippeon1Z/+rDyjc5NiOcI=";
   };
 
   sourceRoot = ".";
