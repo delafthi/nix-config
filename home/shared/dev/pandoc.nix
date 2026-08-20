14458a4cf90881a4fd8f2ab4576798be6bfb256e
Thierry Delafontaine <delafthi@pm.me>
2026-08-20 16:24:35 +0200

    home/dev/pandoc: use typst as the default pdf-engine

--- home/shared/dev/pandoc.nix
+++ home/shared/dev/pandoc.nix
@@ -1,10 +1,17 @@
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [ typst ];
   programs.pandoc = {
     enable = true;
     defaults = {
       citeproc = true;
       metadata.author = "Thierry Delafontaine";
+      pdf-engine = "typst";
       standalone = true;
+      variables = {
+        mainfont = "Libertinus Serif";
+        mathfont = "New Computer Math";
+      };
     };
   };
 }
