20c60fdae8e26a71714512b7f1ddd66998685d77
Thierry Delafontaine <delafthi@pm.me>
2026-09-25 20:51:37 +1200

    home/dev/lang/markdown: use toml generator to create config file

--- home/shared/dev/lang/markdown.nix
+++ home/shared/dev/lang/markdown.nix
@@ -1,6 +1,9 @@
+{ pkgs, ... }:
+let
+  tomlFormat = pkgs.formats.toml { };
+in
 {
-  xdg.configFile."rumdl/rumdl.toml".text = ''
-    [MD013]
-    reflow = true
-  '';
+  xdg.configFile."rumdl/rumdl.toml".source = tomlFormat.generate "rumdl.toml" {
+    MD013.reflow = true;
+  };
 }
